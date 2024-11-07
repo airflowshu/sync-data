71562c47d5d3a95782430469b4a255f8b1f6c24e
airflowshu <657296200@qq.com>
2024-11-07 15:35:33 +0800

    增加启动和关闭程序的脚本

---
+++ scrpits/start.sh
@@ -0,0 +1,9 @@
+# chmod +x start.sh
+# jar包名称
+JAR_NAME="sync-data.jar"
+# 日志文件名称
+LOG_NAME="info.log"
+
+nohup java -jar ${JAR_NAME} --spring.profiles.active=huainan  > logs/${LOG_NAME} 2>&1 &
+echo "${JAR_NAME}启动成功"
+tail -f -n 500 logs/${LOG_NAME}
---
+++ scrpits/stop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#chmod +x stop.sh
+PID=$(ps -ef | grep 'sync-data.jar' | grep -v grep | awk '{print $2}')
+if [ -z "$PID" ]; then
+    echo "No running process found"
+else
+    kill -15 $PID
+    if [ $? -ne 0 ]; then
+        kill -9 $PID
+        echo "sync-data process stopped"
+    fi
+fi
